6afcebf8b0338ebfc3cd61ae15fabcde3d72bdcf
s4rv4d <sarvadshetty@gmail.com>
2019-12-08 17:39:59 +0530

    Initial Commit

---
+++ SwiftUI Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Animations
+//
+//  Created by Sarvad shetty on 12/8/19.
+//  Copyright © 2019 Sarvad shetty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
